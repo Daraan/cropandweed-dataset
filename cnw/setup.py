81301530810ab422f59236ba66890677fa8cb4a5
Daraan <github.blurry@9ox.net>
2023-04-17 20:16:40 +0200

    Download and filter dataset option
    Note: No access over CLI

--- cnw/setup.py
+++ cnw/setup.py
@@ -16,23 +16,26 @@ def parse_arguments():
                         help='directory for downloaded images and annotations')
     parser.add_argument('--no_mapping', dest='with_mapping', action='store_false', default=True,
                         help='skip mapping for predefined dataset variants')
+
+                                     
     return parser.parse_args()
 
 
-def setup(data_root, with_mapping, datasets=datasets.DATASETS):
+def setup(data_root, with_mapping, download=True, datasets=datasets.DATASETS):
     url_prefix = 'https://vitro-testing.com/wp-content/uploads/2022/12/'
 
     os.makedirs(data_root, exist_ok=True)
 
-    for file_name in tqdm(
-            ['cropandweed_annotations', 'cropandweed_images1of4', 'cropandweed_images2of4', 'cropandweed_images3of4',
-             'cropandweed_images4of4'], desc='downloading and extracting files'):
-        response = requests.get(f'{url_prefix}{file_name}.tar', stream=True)
-        archive = tarfile.open(fileobj=response.raw, mode='r|')
-        archive.extractall(data_root)
+    if download:
+        for file_name in tqdm(
+                ['cropandweed_annotations', 'cropandweed_images1of4', 'cropandweed_images2of4', 'cropandweed_images3of4',
+                 'cropandweed_images4of4'], desc='downloading and extracting files'):
+            response = requests.get(f'{url_prefix}{file_name}.tar', stream=True)
+            archive = tarfile.open(fileobj=response.raw, mode='r|')
+            archive.extractall(data_root)
 
-    shutil.move(os.path.join(data_root, 'bboxes'), os.path.join(data_root, 'CropAndWeed'))
-    shutil.move(os.path.join(data_root, 'CropAndWeed'), os.path.join(data_root, 'bboxes', 'CropAndWeed'))
+        shutil.move(os.path.join(data_root, 'bboxes'), os.path.join(data_root, 'CropAndWeed'))
+        shutil.move(os.path.join(data_root, 'CropAndWeed'), os.path.join(data_root, 'bboxes', 'CropAndWeed'))
 
     if with_mapping:
         for dataset in datasets:
